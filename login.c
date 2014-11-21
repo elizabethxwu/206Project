38694ca625f3c3fe005187f92040ee6d2bde2178
Elizabeth Wu <elizabethwu@wpa036250.wireless.mcgill.ca>
2014-11-21 09:38:28 -0500

    fixed string comparison

--- login.c
+++ login.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXLEN 1024
+#define MAXLEN 128
 
 void unencode(char *src, char *last, char *dest);
 void login(char *user);
-void display();
+void displayCatalogue();
+void displayError();
 
 int main(void) 
-{
-//    setenv("QUERY_STRING", "username=eliwu18&password=password", 100);
-    
+{    
     char *user_input;
     char *pass_input;
     char *username;
@@ -20,52 +19,51 @@ int main(void)
     char outputArray[MAXLEN];
     char line[MAXLEN];
 
-    int n = atoi(getenv("CONTENT_LENGTH"));             //Get length of form input
-//    int n = 34; for testing
+// Assuming CGI is envoked with Method POST, not GET
+    int n = atoi(getenv("CONTENT_LENGTH"));             //Get length of input
     fgets(inputArray, n+1, stdin);                      //Form input is sent as stdin
+
+/* Input is sent in with format:
+   username="username'&password="password"   
+   We need to parse this encoded URL      */
+
     unencode(inputArray, inputArray+n, outputArray);    //Unencode URL
 
+    //Parse the unencoded output array
+    //Create temp pointer to copy line
     char *tmp = malloc(sizeof(char)*1024);
-    tmp = strdup(outputArray);                        //copy unencoded output array
-
-    char *tok = strtok(tmp, "=");                 //split tmp string into first token
+    tmp = strdup(outputArray);                    //Copy unencoded output array
+    char *tok = strtok(tmp, "=");                 //Split tmp string into first token
     user_input = strtok(NULL, "&");               //Set "user_input" to point to second token
-    char *tok2 = strtok(NULL, "=");   
+    char *tok2 = strtok(NULL, "=");               
     pass_input = strtok(NULL, "\n");              //Set "pass_input" to point to third token
 
-    free(tmp);
+    FILE* members = fopen("Members.csv", "rt");    //Open Members.csv file
 
-    FILE* members = fopen("Members.csv", "r");    //make sure first arg is Members.csv
-
-    while (fgets(line, MAXLEN, members))
+   while (fgets(line, MAXLEN, members))
     {
-        char *temp = malloc(sizeof(char)*1024);
-        temp = strdup(line);                        //copy fgets line
-
-        char *token = strtok(temp, ",");              //split temp string into tokens
+        //Create temp pointer to copy line
+        char *temp = malloc(sizeof(char)*MAXLEN);
+        temp = strdup(line);                        
+        
+        char *token = strtok(temp, ",");            //split temp string into tokens
         username = strtok(NULL, ",");               //Set "username" to point to second token
         password = strtok(NULL, "\n");              //Set "password" to point to third token
 
-        if(strcmp(user_input, username) != 0)
-            continue;
-
-        if(strcmp(pass_input, password) != 0)
-            continue;
-
-        else{                                       //Username and password are valid
-            login(username);                        //Login the user
-            display();
-            break;                                  
-        }
-        
-        free(temp);
+        //Username and password are valid
+        if(strcmp(user_input, username)==0){
+            if(strcmp(pass_input, password)==0) {
+                login(username);                        
+                displayCatalogue();                     
+                break;        
+            }
+        }        
+        free(temp);        
     }
 
-    if(feof(members)){                              //Username & Password not found (i.e. not valid)
-        printf( "Content-type: text/html\n\n");     //Display error HTML page
-        printf("<HTML><BODY>\n");
-        printf("<P>ERROR</P>\n"); 
-        printf( "</BODY></HTML>\n");
+    //EOF reached and Username & Password not found (i.e. not valid)
+    if(feof(members)){                              
+        displayError();                             
     }
 
     fclose(members);
@@ -94,9 +92,16 @@ void login(char *user){
     fclose(ptr);
 }
 
-void display(){
+void displayCatalogue(){
     printf( "Content-type: text/html\n\n");        //Display catalogue page, insert hidden field
     printf("<HTML><BODY>\n");
-    printf("<P>Hey! This is my first CGI response!</P>\n"); 
+//Output Catalogue page with hidden field username
+    printf( "</BODY></HTML>\n");
+}
+
+void displayError(){
+    printf( "Content-type: text/html\n\n");     //Display error HTML page
+    printf("<HTML><BODY>\n");
+    printf("<P>ERROR</P>\n"); 
     printf( "</BODY></HTML>\n");
 }
