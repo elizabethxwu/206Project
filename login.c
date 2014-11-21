69223d31bd69ca8d694e8edf1012ca7240d458e2
Elizabeth Wu <elizabethwu@Asia.local>
2014-11-20 22:22:38 -0500

    fixed appending username to loggedin.csv

--- login.c
+++ login.c
@@ -20,22 +20,22 @@ int main(void)
     char outputArray[MAXLEN];
     char line[MAXLEN];
 
-    int n = atoi(getenv("CONTENT_LENGTH"));
+    int n = atoi(getenv("CONTENT_LENGTH"));             //Get length of form input
 //    int n = 34; for testing
-    fgets(inputArray, n+1, stdin);
-    unencode(inputArray, inputArray+n, outputArray);
+    fgets(inputArray, n+1, stdin);                      //Form input is sent as stdin
+    unencode(inputArray, inputArray+n, outputArray);    //Unencode URL
 
     char *tmp = malloc(sizeof(char)*1024);
-    tmp = strdup(outputArray);                        //copy fgets line
+    tmp = strdup(outputArray);                        //copy unencoded output array
 
-    char *tok = strtok(tmp, "=");              //split temp string into tokens
-    user_input = strtok(NULL, "&");               //Set "username" to point to second token
+    char *tok = strtok(tmp, "=");                 //split tmp string into first token
+    user_input = strtok(NULL, "&");               //Set "user_input" to point to second token
     char *tok2 = strtok(NULL, "=");   
-    pass_input = strtok(NULL, "\n");              //Set "password" to point to third token
+    pass_input = strtok(NULL, "\n");              //Set "pass_input" to point to third token
 
     free(tmp);
 
-    FILE* members = fopen("Members.csv", "r");   //make sure first arg is Members.csv
+    FILE* members = fopen("Members.csv", "r");    //make sure first arg is Members.csv
 
     while (fgets(line, MAXLEN, members))
     {
@@ -89,7 +89,7 @@ void unencode(char *src, char *last, char *dest){
 }
 
 void login(char *user){
-    FILE* ptr = fopen("loggedin.csv", "r+");        
+    FILE* ptr = fopen("loggedin.csv", "a+");        //a+ means stream is positioned at EOF for writing     
     fprintf(ptr, "%s\n", user);                     // append user name to loggedin.csv
     fclose(ptr);
 }
